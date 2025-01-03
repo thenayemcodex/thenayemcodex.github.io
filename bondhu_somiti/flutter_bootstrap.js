d337a85434a77c79007f58462a8b5229a45d2b6b
The Nayem Codex <nayeme19@gmail.com>
2025-01-03 08:00:18 -0800

    Bondhu Somiti has been finalized

--- bondhu_somiti/flutter_bootstrap.js
+++ bondhu_somiti/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "169442965"
+    serviceWorkerVersion: "3768828071"
   }
 });
